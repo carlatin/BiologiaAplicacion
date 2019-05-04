c8214b1f77d908599f7f0ac79ef2a41465a9d84b
carlatin <robinsonemanuelopez@gmail.com>
2019-05-04 11:01:19 -0600

    firebase

--- www/js/app.js
+++ www/js/app.js
@@ -74,6 +74,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     templateUrl:'templates/info.html'
   })
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/info');
+  $urlRouterProvider.otherwise('/agua');
 
 });
